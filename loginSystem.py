e52d227456beac47e86d2e1fc0841faf3dea4716
wishahy <wishahygamer@gmail.com>
2023-08-20 18:37:47 +0300

    wishahy part while true

--- loginSystem.py
+++ loginSystem.py
@@ -16,7 +16,7 @@ def fun1():
     else:
         return -1
 #for register
-def fun2():
+def register():
     x = input(constantMessage1)
     if checkingCaseOfUserNameInDataBase(x):
         return -1
@@ -24,34 +24,34 @@ def fun2():
     z = input("Now enter your secrete phrase for safekeeping: ")
     database[x] = {"password": y, "secret": z}
     return 1
-def printingOutAllChoicesAndReturnUserChoice():
+def choice():
     print("\nWhat would you like to do?")
     print("1) ")
     print("2) ")
     print("3) Exit\n")
-    v = int(input("> "))
+    answer = int(input("> "))
     print("")
-    return v
+    return answer
 while True: #loop runs forever till break
-    c = printingOutAllChoicesAndReturnUserChoice()
-    match c:
+    answer = choice()
+    match answer:
         case 1:
 
-            s = fun2()
-            if s == 0:
+            username = register()
+            if username == 0:
                 print("error: username not found")
-            elif s == -1:
+            elif username == -1:
                 print("error: incorrect password")
             else:
-                print(f"\nWelcome back, {s}!")
+                print(f"\nWelcome back, {username}!")
                 print("Your secret phrase is:")
-                print(database[s]["secret"])
+                print(database["username"]["secret"])
             continue
         case 2:
-            s=fun1()
-            if s == 1:
+            username=fun1()
+            if username == 1:
                 print("\nSuccessfuly Registered!")
-            elif s==-1:
+            elif username==-1:
                 print("error: username already exists")
             continue
         case 3:
